6cfadc97db46c79267ab60221bd9cbeb4c17e870
jahan <jahan@j-MacBook-Air.home>
2021-03-23 00:10:51 -0400

    page404, about and contact pages created

--- mvc/routes/index.js
+++ mvc/routes/index.js
@@ -5,5 +5,12 @@ var router = express.Router();
 const indexCtrl = require("../controllers/index");
 
 router.get('/', indexCtrl.getHomePage);
+router.get('/posts/:postid', indexCtrl.getBlogPost);
+
+router.get('/about', indexCtrl.getAbout);
+router.get('/contact', indexCtrl.getContact);
+
+router.get('/404', indexCtrl.get404);
+router.get('*', indexCtrl.redirect404);
 
 module.exports = router;
